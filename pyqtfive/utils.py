d3930f06be75cb7a7391d0787f3eefa488310fb2
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-06-11 17:36:20 +0800

    Add error handler functions in utils.py

--- pyqtfive/utils.py
+++ pyqtfive/utils.py
@@ -5,3 +5,13 @@ def buffer():
     
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def handle_value_error():
+    clear_screen()
+    print("Invalid input. Please enter a number.")
+    buffer()
+
+def handle_unexpected_error(e):
+    clear_screen()
+    print(f"An unexpected error occurred: {e}")
+    buffer()
